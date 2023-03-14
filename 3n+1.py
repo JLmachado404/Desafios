b021d8bbf5c6082f947e0455b745a95f7268ed6e
João Lucas <jlmachado4004@gmail.com>
2023-03-14 19:27:24 -0400

    3n+1 continuation

---
+++ 3n+1.py
@@ -0,0 +1,34 @@
+# entrance = [int(x) for x in input("").split()]
+entrance = iter(input, '')
+
+print(entrance)
+   
+
+# x = 0
+# y = 1
+# i = entrance[x]
+# j = entrance[y]
+
+# for m in entrance:
+#   maior = 0
+#   if y <= len(entrance):
+#     i = entrance[x]
+#     j = entrance[y]
+#     for n in range(i,j + 1):
+#         iteração = 1
+#         while n != 1:
+#             if n % 2 == 0:
+#                 n = n /2
+#                 iteração += 1
+#             else:
+#                 n = n * 3  + 1
+#                 iteração += 1
+#         if iteração > maior:
+#             maior = iteração
+#     print(f"{i} {j} {maior}")
+
+#     y += 2
+#     x += 2
+
+
+    
